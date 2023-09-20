bb1b7eb0d54c220d760069620db9038f05df0be4
Tommy Xu <zwxu@umich.edu>
2023-09-20 17:14:51 -0400

    Main updated

--- ToDoApp.py
+++ ToDoApp.py
@@ -5,9 +5,9 @@ class ToDoModel:
     def add_task(self, task):
         self.tasks.append(task)  # Add a task to the list
 
-#    def delete_task(self, task_index):
-#        if 0 <= task_index < len(self.tasks):
-#            del self.tasks[task_index]  # Delete the task at the specified index
+    def delete_task(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            del self.tasks[task_index]  # Delete the task at the specified index
 
     def get_tasks(self):
         return self.tasks  # Return the list of tasks
@@ -18,9 +18,9 @@ class ToDoView:
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")  # Print each task with its indexpyth
 
-#    def sort_todo_list(self, tasks):
-#        sorted_tasks = sorted(tasks)  # Sort the tasks alphabetically
-#        self.print_todo_list(sorted_tasks)  # Print the sorted to-do list
+    def sort_todo_list(self, tasks):
+        sorted_tasks = sorted(tasks)  # Sort the tasks alphabetically
+        self.print_todo_list(sorted_tasks)  # Print the sorted to-do list
 
 class ToDoController:
     def __init__(self, model, view):
@@ -47,20 +47,22 @@ def main():
     controller = ToDoController(model, view)
 
     controller.add_task("Go shopping")
-    controller.add_task("Complete work project")
-    controller.add_task("Attend school meeting")
+    controller.add_task("Complete school project")
+    controller.add_task("Attend work meeting")
+    controller.add_task("Complete work project at 10pm")
+    controller.add_task("Attend school meeting at 9am")
 
     # Display the initial To-Do list
     controller.display_todo_list()
 
     # Sort and display the To-Do list
- #   controller.sort_todo_list()
+    controller.sort_todo_list()
 
     # Delete a task (e.g., the second task, index 1)
- #   controller.delete_task(1)
+    controller.delete_task(1)
 
     # Display the updated To-Do list after deletion
-    # controller.display_todo_list()
+    controller.display_todo_list()
 
 if __name__ == "__main__":
     main()
